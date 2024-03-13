619aba83d965508370a7044e849353c6e6c3f3bf
georgios.vassos1 <georgios.vassos1@maersk.com>
2024-03-13 10:12:35 +0100

    Update to sync the changes in the PKG.

--- demo/optex.R
+++ demo/optex.R
@@ -2,9 +2,10 @@ library(TLPR)
 
 ## Dynamic program
 env <- new.env()
-input_configuration(env)
-simulate_auction(env)
-initialize(env, rate = 0.4)
+# input_configuration(env)
+# simulate_auction(env)
+# initialize(env, rate = 0.4)
+generate_cssap(env, rate = 10.0, tau = 4L, nCS = 1L, nCO = 1L, nI = 1L, nJ = 1L)
 model <- create_model(env)
 
 get_from_routes(env)
@@ -17,11 +18,11 @@ get_state_indices(env, max.S, inc.S)
 
 ## Exogenous state vector
 Q <- list(
-  "vals" = seq(0L, env$R, by = 2L),
+  "vals" = seq(0L, env$R %/% env$tau, by = 10L),
   "prob" = c(0.2, 0.4, 0.4)
 )
 D <- list(
-  "vals" = seq(0L, env$R, by = 2L),
+  "vals" = seq(0L, env$R %/% env$tau, by = 10L),
   "prob" =c(0.2, 0.4, 0.4, 0.15)
 )
 W <- list(
@@ -32,9 +33,20 @@ W <- list(
 # Homogeneity: all lanes same cost of transportation in the spot
 get_scenario_space(Q, D, W)
 
-## Action space
-A_ <- seq(0L, env$nI*(max.S+max(Q$vals)), by = 4L)
+## Action space 
+# A_ <- seq(0L, env$nI*(max.S+max(Q$vals)), by = 5L)
+A_ <- seq(0L, max(env$Cb+env$Co), by = 5L)
 nA <- length(A_)
+
+run_scenarios(env, 1L, 1L, nSdx, 1L, adj.w)
+
+adj.w <- c(1L, 1L)
+K     <- seq(nOmega)
+result <- matrix(NA, nrow = env$tau*nSdx*nA*nOmega, ncol = 5L)
+for (t in seq(env$tau)) {
+  print(t)
+  result[(t-1L)*nSdx*nA*nOmega+seq(nSdx*nA*nOmega),] <- run_scenarios(env, t, 1L, nSdx, K, adj.w)
+}
 
 ## Parallel run
 chunkup <- function(n, k) {
@@ -44,36 +56,67 @@ chunkup <- function(n, k) {
   chunks
 }
 
-n_chunks <- 48L
+n_chunks <- 12L
 chunks   <- chunkup(nSdx, n_chunks)
 
 library(foreach)
 library(doParallel)
+
+# adj.w <- c(1L, nSI, nSI^2L, (nSI^2L)*nSJ)
+# K <- which((rowSums(Phi.t[,1L:2L]) > 4L) & (rowSums(Phi.t[,3L:4L]) > 4L))
+
+adj.w <- c(1L, 1L)
+K     <- seq(nOmega)
+lenK  <- length(K)
 
 cores <- detectCores()
 cl    <- makeCluster(cores-4L) # not to overload your computer
 clusterExport(cl, c("model", "max.S", "SI_", "SJ_", "Sdx", "A_", "nA", "Phi.t"))
 registerDoParallel(cl)
 
-adj.w <- c(1L, nSI, nSI^2L, (nSI^2L)*nSJ)
-
-t <- 1L
-K <- c(150L,151L)
-foreach (idx = seq(n_chunks), .packages = c('gurobi','TLPR'), .combine = 'rbind') %dopar% {
-  start <- chunks[idx]
-  end   <- chunks[idx+1L]-1L
-  run_scenarios(env, t, start, end, K, adj.w)
-} -> result
+result <- matrix(NA, nrow = env$tau*nSdx*nA*lenK, ncol = 5L)
+for (t in seq(env$tau)) {
+  print(t)
+  foreach (idx = seq(n_chunks), .packages = c('gurobi','TLPR'), .combine = 'rbind') %dopar% {
+    start <- chunks[idx]
+    end   <- chunks[idx+1L]-1L
+    run_scenarios(env, t, start, end, K, adj.w)
+  } -> result[(t-1L)*nSdx*nA*lenK+seq(nSdx*nA*lenK),]
+}
 
 stopCluster(cl)
 
+# mget(ls(), envir = .GlobalEnv) |> saveRDS("~/Desktop/instance001.R")
+
+active <- which(complete.cases(result))
+
+result <- cbind2(result, NA)
+for (t in seq(env$tau)) {
+  result[(t-1L)*nSdx*nA*lenK+seq(nSdx*nA*lenK),6L] <- t
+}
+transit <- result
+
+tilde_phidx <- 12L # Fix scenario
+
+V <- matrix(NA, nrow = env$tau+1L, ncol = nSdx)
+V[env$tau+1L,] <- 0.0
+Q <- matrix(NA, nrow = env$tau, ncol = nSdx*nA)
 ## Dynamic Programming
-for (t in seq(tau,1L)) {
+for (t in seq(env$tau,1L)) {
+  print(t)
   for (i in seq(nSdx)) {
     for (j in seq(nA)) {
-      next_i <- transit[(i-1L)*nOmega*nA+(j-1L)*nOmega+seq(nOmega),]
-      Q[t,(i-1L)*nA+j] <- u[t,(i-1L)*nA+j] + V[t+1L,(next_i-1L)*nA+j] %*% PPhi.t
+      position <- (((t-1L)*nSdx+(i-1L))*nA+(j-1L))*lenK
+      next_s <- transit[position + seq(lenK),1L]
+      cost.t <- transit[position + tilde_phidx,2L] # Fix scenario
+      scnidx <- transit[position + seq(lenK),5L]
+      prob   <- PPhi.t[scnidx] / sum(PPhi.t[K])
+      Q[t,(i-1L)*nA+j] <- - cost.t + c(V[t+1L,next_s] %*% prob)
     }
-    V[t,i] <- max(Q[t,(i-1L)*nA+seq(nA)])
+    V[t,i] <- max(Q[t,(i-1L)*nA+seq(nA)], na.rm = TRUE)
   }
 }
+
+sum(apply(t(matrix(Q[1L,], nrow = nA)), 1L, function(x) which.max(x)) != 1L)
+
+      
--- demo/simex.R
+++ demo/simex.R
@@ -1,8 +1,21 @@
 library(TLPR)
 
 env <- new.env()
-generate_cssap(env, tau = 12L, nCO = 1L)
-model <- create_model(env)
+generate_cssap(env, rate = 10.0, tau = 12L, nCS = 5L, nCO = 1L, nB = 3L, nI = 4L, nJ = 3L)
+
+env$alpha <- c(rep(1.0, env$nI), rep(c(1.0, 2.0), each = env$nJ))
+
+# Choose the effective constraints
+#   Constraints 2L and 3L depend on the state of the system.
+#   This entails significant computational burden in evaluating the portfolio contract.
+Idx    <- c(1L,4L)
+rhs_dx <- unlist(list(
+  seq(env$nCS+env$nCO), 
+  env$nCS+env$nCO+seq(env$nI),
+  env$nCS+env$nCO+env$nI+seq(env$nJ),
+  env$nCS+env$nCO+env$nI+env$nJ+1L)[Idx])
+
+model <- create_model(env, Idx)
 
 policy <- list(
   "myopic" = optimal_assignment,
@@ -15,38 +28,39 @@ args <- list(
   obj_ = NULL,
   rhs_ = NULL,
   n = NULL,
-  k = env$nvars
+  k = env$nvars,
+  x = NULL
 )
 
 exog <- list(
-  "Q" = list("func"=rpois, "params"=list('n'=env$tau*env$nI, 'lambda'=40L)),
-  "D" = list("func"=rpois, "params"=list('n'=env$tau*env$nI, 'lambda'=40L))
+  "Q" = list("func"=rpois, "params"=list('n'=env$tau*env$nI, 'lambda'=10L)),
+  "D" = list("func"=rpois, "params"=list('n'=env$tau*env$nJ, 'lambda'= 5L))
 )
 
 get_from_routes(env)
 get_to_routes(env)
 
-N <- 30L
+N <- 50L
 costs <- matrix(NA, nrow = env$tau, ncol = npi*N)
 i <- 1L
 while (i <= N) {
   print(i)
-  result <- simulate_system(env, policy, args, exog)
+  result <- simulate_system(env, policy, args, exog, rhs_dx, correction = TRUE)
   if (result$status) {
     costs[,(i-1L)*npi+seq(npi)] <- result$cost
     i <- i + 1L
   }
 }
 
-stats_ <- do.call(cbind, lapply(seq(npi), function(x) compute_stats(costs[,seq(x, npi*N, by = npi)], N)))
-matplot(stats_, type = 'l', lwd = rep(c(2L,1L,1L), 2L), lty = 1L, pch = 19L, ylab = "Cumulative Cost", col = rep(c(1L,2L), each = 3L))
+stats_ <- do.call(cbind, lapply(seq(npi), function(x) compute_stats(costs[,seq(x, npi*N, by = npi)], N = 1L)))
+matplot(stats_, type = 'l', lwd = rep(c(2L,1L,1L), 2L), lty = 1L, pch = 19L, ylab = "Cumulative Cost", col = rep(c(1L,2L,3L), each = 3L))
 
 ## Single run plots
 matplot(apply(result$cost, 2L, cumsum), type = 'b', lwd = 2L, lty = 1L, pch = 19L, ylab = "Cumulative Cost")
 
 pdx <- 1L
 idx <- outer((seq(env$tau)-1L)*env$nI, seq(env$nI), '+')
-i <- 1L
+i <- 2L
 plot(result$S.I[idx[,i],pdx], type = 's', ylim = c(min(result$S.I[,pdx],result$X.I[,pdx]), max(result$S.I[,pdx],result$X.I[,pdx])))
 lines(result$X.I[idx[,i],pdx], type = 's', col = 2L)
 lines(result$Q[idx[,i]], type = 's', col = 3L)
