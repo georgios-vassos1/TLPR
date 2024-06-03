03f5cbfbdddfb8905df4763852352743c4e0bc58
georgios.vassos1 <georgios.vassos1@maersk.com>
2024-06-03 17:37:33 +0200

    New Rcpp utilities (test)

--- R/R2STL.R
+++ R/R2STL.R
@@ -8,7 +8,22 @@
 #' @useDynLib TLPR
 #' @importFrom Rcpp sourceCpp
 #' @export
-createAndPassList <- function(list_of_vectors) {
+createAndPassList <- function(list_of_vectors, is_map = FALSE, key_type = "int") {
     # Call the C++ function to process the list
-    invisible(.Call('_TLPR_processListSEXP', list_of_vectors))
+    invisible(.Call('_TLPR_processListSEXP', list_of_vectors, is_map, key_type))
+}
+
+#' Process R matrix object
+#'
+#' This function takes an R matrix object and passes it to a C++ function to be
+#' processed.
+#'
+#' @examples
+#' passRmatrixToCPP()
+#' @useDynLib TLPR
+#' @importFrom Rcpp sourceCpp
+#' @export
+passRmatrixToCPP <- function(R_matrix) {
+    # Call the C++ function to process the list
+    .Call('_TLPR_r_to_eigen', R_matrix)
 }
--- src/R2STL.cpp
+++ src/R2STL.cpp
@@ -1,8 +1,45 @@
-#include <Rcpp.h>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+#include <Rcpp.h>
 
 using namespace Rcpp;
+
+// Function to convert R list of vectors to std::map<int, std::vector<int>>
+template <typename KeyType, typename ValueType>
+std::map< KeyType, std::vector<ValueType> > convertListToMap(SEXP inputList) {
+    std::map< KeyType, std::vector<ValueType> > result;
+
+    // Ensure the input is a list
+    if (!Rf_isNewList(inputList)) {
+        Rcpp::stop("Input must be a list");
+    }
+
+    Rcpp::List list(inputList);
+    bool useIndices = Rf_isNull(list.names());
+    // std::cout << "useIndices: " << useIndices << std::endl;
+    Rcpp::CharacterVector names;
+    if (!useIndices) {
+        names = list.names();
+    }
+
+    for (int i = 0; i < list.size(); i++) {
+        KeyType key;
+        if (std::is_same<KeyType, int>::value) {
+            key = i;
+        } else {
+            key = Rcpp::as<KeyType>(names[i]);
+        }
+
+        SEXP valuesSEXP = list[i];
+        Rcpp::NumericVector values(valuesSEXP);
+        std::vector<ValueType> vec(values.begin(), values.end());
+
+        result[key] = vec;
+    }
+
+    return result;
+}
 
 // Function to convert R list of vectors to std::vector<std::vector<int>>
 std::vector< std::vector<int> > convertListToVector(SEXP list_sexp) {
@@ -18,6 +55,15 @@ std::vector< std::vector<int> > convertListToVector(SEXP list_sexp) {
     return vec;
 }
 
+// This function reads a matrix from R and converts it to an Eigen matrix
+// [[Rcpp::export]]
+Eigen::MatrixXd r_to_eigen(const Eigen::MatrixXd& mat) {
+    // Here, mat is already an Eigen::MatrixXd object because RcppEigen handles the conversion
+    // You can now perform any Eigen operations on mat
+    Eigen::MatrixXd result = mat.transpose(); // Example operation: transpose
+    return result;
+}
+
 // Function to print the std::vector<std::vector<int>>
 void printVector(const std::vector< std::vector<int> >& vec) {
     for (const auto& sub_vec : vec) {
@@ -28,9 +74,33 @@ void printVector(const std::vector< std::vector<int> >& vec) {
     }
 }
 
+// Function to print the std::map<int, std::vector<int>>
+template <typename KeyType, typename ValueType>
+void printMap(const std::map< KeyType, std::vector<ValueType> >& map) {
+    for (const auto& pair : map) {
+        std::cout << pair.first << ": ";
+        for (const auto& val : pair.second) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 // Wrapper function to be called from R
 // [[Rcpp::export]]
-void processListSEXP(SEXP list_sexp) {
-    std::vector< std::vector<int> > vec = convertListToVector(list_sexp);
-    printVector(vec);
+void processListSEXP(SEXP list_sexp, bool is_map, const std::string KeyTypeArg) {
+    if (is_map) {
+        if (KeyTypeArg == "int") {
+            std::map< int, std::vector<float> > map = convertListToMap<int, float>(list_sexp);
+            printMap<int, float>(map);
+        } else if (KeyTypeArg == "std::string") {
+            std::map< std::string, std::vector<float> > map = convertListToMap<std::string, float>(list_sexp);
+            printMap<std::string, float>(map);
+        } else {
+            Rcpp::stop("KeyTypeArg must be either 'int' or 'std::string'");
+        }
+    } else {
+        std::vector< std::vector<int> > vec = convertListToVector(list_sexp);
+        printVector(vec);
+    }
 }
