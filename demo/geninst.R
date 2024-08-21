d3d4993c22e4c7b92e79aacd545c4fe9a3490e55
GVA <georgios.vassos1@maersk.com>
2024-08-21 13:33:36 +0200

    Cleaning

--- demo/geninst.R
+++ demo/geninst.R
@@ -1,20 +1,7 @@
 library(TLPR)
 
-get_adjustment_weights <- function(env) {
-  # Generate powers of nSI from 1 to nI
-  si_powers <- with(env, nSI ^ (1L:nI))
-
-  if (env$nJ == 1L) return(c(1L, si_powers))
-  # Calculate the base value (nSI ^ nI)
-  base_value <- with(env, nSI ^ nI)
-  # Generate products of base_value and powers of nSJ from 0 to (nJ - 1)
-  sj_powers <- base_value * with(env, nSJ ^ (1L:(nJ - 1L)))
-  # Combine the results
-  c(1L, si_powers, sj_powers)
-}
-
 env <- new.env()
-generate_cssap(env, rate = 4.0, tau = 4L, nB = 5L, nCS =10L, nCO = 1L, nI = 1L, nJ = 1L)
+generate_cssap(env, rate = 4.0, tau = 12L, nB = 5L, nCS =10L, nCO = 1L, nI = 1L, nJ = 1L)
 
 # apply(env$Cb, 1L, function(x) x, simplify = "array") -> env$Cbx
 
@@ -38,33 +25,39 @@ data.frame(Carrier = names(env$winner)[env$carriers[seq(env$nL_)]], CTb = env$CT
 
 with(
   env, {
-    max.S <- R <- 10L
+    max.S <- 10L
+    R     <- 10L
 
     nSI  <- max.S + 1L
     nSJ  <- 2L * max.S + 1L
     nSdx <- (nSI^nI)*(nSJ^nJ)
-    nA   <- 4L
+    nA   <- max.S + 1L
+
     alpha <- c(rep(5.0, env$nI), rep(c(4.0, 8.0), each = env$nJ))
+
     Q <- list(
       "vals" = c(0L, 4L, 8L),
-      "prob" = c(0.4, 0.3, 0.3)
+      "prob" = c(0.1, 0.7, 0.2)
     )
     D <- list(
       "vals" = c(0L, 4L, 8L),
-      "prob" = c(0.25, 0.25, 0.5)
+      "prob" = c(0.1, 0.5, 0.4)
     )
     W <- list(
       "vals" = c(7.0, 22.0),
       "prob" = dnorm(c(7.0, 22.0), 18.0, 12.0) / sum(dnorm(c(7.0, 22.0), 18.0, 12.0))
     )
+
     nQ <- length(Q$vals)
     nD <- length(D$vals)
     nW <- length(W$vals)
+
     nOmega <- (nQ ^ nI) * (nD ^ nJ) * (nW ^ nCO)
+
     stateKeys <- get_adjustment_weights(env)
     flowKeys <- c(1L, nQ ^ (1L:(nI+nJ-1L)))
   }
 )
 
 json <- jsonlite::toJSON(sapply(names(env), get, envir = env), pretty = TRUE)
-write(json, file = "/Users/gva/drayage/TLPR/src/instance1x1_001.json")
+write(json, file = paste0("~/drayage/TLPR/src/instances/instance", env$nI, 'x', env$nJ, '_', env$tau, "_001.json"))
