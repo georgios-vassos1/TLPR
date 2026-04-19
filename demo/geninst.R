b27b97567ee9d976f33a5996df514800cb365f9d
GVA <georgios.vassos1@maersk.com>
2026-04-19 17:00:41 +0200

    chore(demo): archive 12 obsolete/VFA-era scripts

--- demo/geninst.R
+++
@@ -1,8 +0,0 @@
-library(TLPR)
-
-env <- generate_instance(
-  nI = 2L, nJ = 1L, tau = 4L,
-  nB = 5L, nCS = 10L, nCO = 1L, rate = 4.0,
-  seed = 42L,
-  path = "~/drayage/TLPR/src/instances/instance2x1_4_001.json"
-)
